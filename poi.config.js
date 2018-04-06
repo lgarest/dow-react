28a83bdd6465b89efeaf60f7ad8e6cb56acf87d8
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-06 11:56:22 +0200

    Set package.json

---
+++ poi.config.js
@@ -0,0 +1,8 @@
+module.exports = (options, req) =>({
+  entry: './src/index.js',
+  jsx: 'react',
+  // Other options
+})
+
+// Note that you can directly export an object too:
+// module.exports = { port: 5000 }
