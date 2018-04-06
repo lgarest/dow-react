d23ed28bab4f5f52dba838c14a4dfe2df7322f2d
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-06 13:18:00 +0200

    Add eslint support based on Airbnb styleguides

--- poi.config.js
+++ poi.config.js
@@ -1,10 +1,11 @@
-module.exports = (options, req) =>({
-  entry: './src/index.js',
+// module.exports = (options, req) => ({
+module.exports = () => ({
+  entry: './src/index.jsx',
   presets: [
-    require('poi-preset-react')()
+    require('poi-preset-react')(),
   ],
   // Other options
-})
+});
 
 // Note that you can directly export an object too:
 // module.exports = { port: 5000 }
--- src/button.jsx
+++ src/button.jsx
@@ -1,6 +1,20 @@
-const Button = (props) => (
-  <button className={`button ${props.red ? 'button--red' : ''}`} onClick={props.action}>
-  {props.children}
+/* 3rd party imports */
+import React from 'react';
+import { func, node } from 'prop-types';
+
+
+const Button = ({ click, children }) => (
+  <button onClick={click}>
+    {children}
   </button>
 );
+
+Button.propTypes = {
+  click: func,
+  children: node.isRequired,
+};
+Button.defaultProps = {
+  click: () => {},
+};
+
 export default Button;
