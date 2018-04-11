b197b14ecc96c14e402adabaa04fca96fe313ace
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-11 23:09:47 +0200

    WIP locations logic

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -5,25 +5,58 @@ import { Game } from 'boardgame.io/core';
 /* Relative imports */
 import Board from '../components/board';
 
+const zombieEntrance = () => {
+  let zombie = false;
+  let barricaded = false;
+  const zombieEnters = () => (zombie
+    ? false
+    : barricaded
+      ? (() => { barricaded = false; })()
+      : (() => { zombie = true; })()
+  );
+  return {
+    zombie,
+    barricaded,
+    zombieEnters,
+  };
+};
+
+const Locations = (ctx) => {
+  const locationsConfig = [{
+    name: 'Police station',
+    searchDeck: [1, 3, 5, 7, 9],
+    players: [],
+    maxPlayers: 3,
+    initialEntrances: 3,
+    entrances: [],
+  }, {
+    name: 'Gas station',
+    searchDeck: [2, 4, 6, 8, 10],
+    players: [],
+    maxPlayers: 2,
+    initialEntrances: 2,
+    entrances: [],
+  }];
+
+  return {
+    initialSetup: () =>
+      locationsConfig
+        .map(location => ({
+          ...location,
+          entrances: Array(location.initialEntrances).fill(0).map(zombieEntrance),
+          searchDeck: ctx.random.Shuffle(location.searchDeck),
+        })),
+  };
+};
+
 const MainGame = Game({
-  setup: () => ({
-    round: 0,
-    morale: 0,
-    goal: {
-      name: 'LEAVE IT ALL',
-      victoryCondition: () => undefined,
-    },
-    crisis: {},
-    garbage: {},
-    colony: {},
-    cells: Array(9).fill(null)
-  }),
+  setup: ctx => ({ locations: Locations(ctx).initialSetup() }),
 
   moves: {
-    clickCell(G, ctx, id) {
-      let cells = [...G.cells]; // don't mutate original state.
-      cells[id] = ctx.currentPlayer;
-      return { ...G, cells }; // don't mutate original state.
+    moveToLocation(G, ctx, id) {
+      const locations = [...G.locations]; // don't mutate original state.
+      locations[id].players = locations[id].players.concat(ctx.currentPlayer);
+      return { ...G, locations }; // don't mutate original state.
     },
   },
 });
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import Colony from './colony';
+
+/* eslint-disable */
+
+const Entrance = props => (
+  <span>
+    {
+      props.zombie
+        ? '[Z]'
+        : props.barricaded
+        ? '[B]'
+        : '[ ]'
+    }
+  </span>
+);
+
+const Location = props => (
+  <div onClick={props.click} something={console.log('rops', props)}>
+    <span>{props.name} </span>
+    <span>Players: {props.players} </span>
+    <span>Max players: {props.maxPlayers} </span>
+    <p>Search deck: {props.searchDeck} </p>
+    <p>Entrances: {
+      props.entrances.map((entrance,idx) =>
+        <Entrance key={idx} {...entrance} />
+      )
+    } </p>
+  </div>
+);
 
 const Board = props => (
   <div>
-    <Colony {...props} />
-    <div>-------------</div>
-    <div>Police Station</div>
-    <div>Supermarket</div>
-    <div>School</div>
-    <div>Library</div>
-    <div>Gas Station</div>
-    <div>Hospital</div>
+    {props.G.locations.map((cell, idx) =>
+      <Location
+        key={idx}
+        click={() => props.moves.moveToLocation(idx)}
+        {...props.G.locations[idx]}
+      />
+    )}
   </div>
 );
 
 export default Board;
+/* eslint-enable */
