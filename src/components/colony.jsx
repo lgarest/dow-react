cda52e00d32426a5c75d071777d9fa16dec556af
Luis Miguel Fernández <luis.fernandez@elementsinteractive.es>
2018-04-11 22:50:20 +0200

    Created initial colony tile which displays morale and round. Also created styleless numeric tracks for morale and round. Created empty template for location tile.

---
+++ src/components/colony.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Track from './numeric-track';
+import Tile from './location-tile';
+
+// @todo: Change this to a proper grid layout with areas
+const ColonyTile = styled(Tile)`
+  background: rgba(211, 0 , 0, 0.4);
+`;
+
+const Colony = props => (
+  <ColonyTile>
+    <Track name="Morale" order="desc" value={props.G.morale} />
+    <Track name="Rounds" order="asc" value={props.G.round} />
+  </ColonyTile>
+);
+
+export default Colony;
---
+++ src/components/location-tile.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const LocationTile = styled.div`
+  display: grid;
+  background: rgba(211,211,211, 0.4);
+  padding: 1rem;
+`;
+
+export default LocationTile;
