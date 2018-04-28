0e298822944b4c898d7c07c051f2750731e9241d
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-28 15:52:24 +0200

    Remove useless app folder
    Now the 'app' code is inside the entry point of the app in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 /* 3rd party imports */
 import React from 'react';
 import { render } from 'react-dom';
+import { Client } from 'boardgame.io/react';
 
 /* relative imports */
-import App from './app';
+import MainGame from './main-game';
+import Board from './components/board';
+
+const App = Client({
+  game: MainGame,
+  board: Board,
+});
 
 
 render(<App />, document.getElementById('app'));
